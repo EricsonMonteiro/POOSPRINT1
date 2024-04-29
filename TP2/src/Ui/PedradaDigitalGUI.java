31765cd0ca16372f04c7a6cc2aa2a6c3d32e507d
EricsonMonteiro <esmonteiro@uta.cv>
2024-04-29 17:23:29 +0200

    1

---
+++ TP2/src/Ui/PedradaDigitalGUI.java
@@ -0,0 +1,13 @@
+package Ui;
+
+import javax.swing.*;
+
+public class PedradaDigitalGUI extends JFrame {
+    public static final JTextArea boardComando= new JTextArea();
+
+
+    public static final JTextField campoComando = new JTextField();
+
+
+
+}
